4230e7aef7a370acd82d214cf755908bd90c1c44
sajanajk <120516209+sajanajk@users.noreply.github.com>
2023-03-08 08:53:39 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Sajana J K | Cyber Security Guy</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hello! I'm Sajana. Don't worry, I'm from IT" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
